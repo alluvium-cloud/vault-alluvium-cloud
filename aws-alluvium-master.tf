50251ffceb7855f08e5059647ab092e8a2024898
Eric Reeves <eric@alluvium.com>
2023-09-24 18:49:48 -0500

    Re-add module

--- aws-alluvium-master.tf
+++ aws-alluvium-master.tf
@@ -1,14 +1,14 @@
-# module "tfc-auth-alluvium-master" {
-#   source = "./modules/terraform-vault-terraform-cloud-jwt-auth"
+module "tfc-auth-alluvium-master" {
+  source = "./modules/terraform-vault-terraform-cloud-jwt-auth"
 
-#   terraform = local.terraform
-#   vault     = local.vault
+  terraform = local.terraform
+  vault     = local.vault
 
-#   roles = [
-#     {
-#       workspace_name = "tfc-jwt-test"
-#       project_name   = "Alluvium Cloud"
-#       token_policies = ["aws/alluvium-master/traditional", "aws/alluvium-master/admin"]
-#     }
-#   ]
-# }
+  roles = [
+    {
+      workspace_name = "tfc-jwt-test"
+      project_name   = "Alluvium Cloud"
+      token_policies = ["aws/alluvium-master/traditional", "aws/alluvium-master/admin"]
+    }
+  ]
+}
