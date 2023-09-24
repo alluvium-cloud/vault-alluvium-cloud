22c3c37078d7d33b3467d4a9605b1acb035e0b09
Eric Reeves <eric@alluvium.com>
2023-09-24 18:48:43 -0500

    drop vault config

--- aws-alluvium-cloud.tf
+++ aws-alluvium-cloud.tf
@@ -1,14 +1,14 @@
-module "tfc-auth-alluvium-cloud" {
-  source = "./modules/terraform-vault-terraform-cloud-jwt-auth"
+# module "tfc-auth-alluvium-cloud" {
+#   source = "./modules/terraform-vault-terraform-cloud-jwt-auth"
 
-  terraform = local.terraform
-  vault     = local.vault
+#   terraform = local.terraform
+#   vault     = local.vault
 
-  roles = [
-    {
-      workspace_name = "alluvium-infrastructure"
-      project_name   = "Alluvium Cloud"
-      token_policies = ["aws/alluvium-cloud/traditional", "aws/alluvium-cloud/admin"]
-    }
-  ]
-}
+#   roles = [
+#     {
+#       workspace_name = "alluvium-infrastructure"
+#       project_name   = "Alluvium Cloud"
+#       token_policies = ["aws/alluvium-cloud/traditional", "aws/alluvium-cloud/admin"]
+#     }
+#   ]
+# }
